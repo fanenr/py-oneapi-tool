86df467ca7e0e9b7aad81b0d73ce6ccc2f89340e
Arthur63 <arthur63@163.com>
2024-10-25 21:16:42 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import sqlite3
+from nicegui import ui
+
+conn = sqlite3.connect("one-api.db")
+
+with ui.header().classes("justify-center"):
+    ui.label("Api Tool").classes("text-xl")
+
+with ui.row().classes("w-full"):
+    cursor = conn.cursor()
+
+    with ui.list().props("bordered separator"):
+        ui.item_label("Tables").props("header").classes("text-bold")
+        ui.separator()
+
+        cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
+
+        for row in cursor.fetchall():
+            with ui.item().classes("items-center"):
+                ui.item_label(row[0])
+
+    cols = [
+        {"name": "id", "label": "ID", "field": "id"},
+        {"name": "name", "label": "Name", "field": "name"},
+        {"name": "url", "label": "BaseUrl", "field": "url"},
+        {"name": "key", "label": "ApiKey", "field": "key"},
+        {"name": "speed", "label": "Speed", "field": "speed"},
+    ]
+    params = {
+        "align": "left",
+        "sortable": True,
+        "headerClasses": "text-primary",
+    }
+    with ui.table(rows=[], columns=cols, column_defaults=params).classes(
+        "flex-1"
+    ) as table:
+        cursor.execute("SELECT id, name, base_url, key, response_time FROM channels;")
+
+        for row in cursor.fetchall():
+            table.add_row(
+                {
+                    "id": row[0],
+                    "url": row[2],
+                    "key": row[3],
+                    "name": row[1],
+                    "speed": row[4] / 1000,
+                }
+            )
+
+    cursor.close()
+
+ui.run(port=8888)
