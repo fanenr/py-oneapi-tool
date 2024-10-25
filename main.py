d4cedd55aa43a7147cdf96ab7f62fb5c5dab87ee
Arthur63 <arthur63@163.com>
2024-10-25 21:31:03 +0800

    remove tables

--- main.py
+++ main.py
@@ -6,48 +6,43 @@ conn = sqlite3.connect("one-api.db")
 with ui.header().classes("justify-center"):
     ui.label("Api Tool").classes("text-xl")
 
-with ui.row().classes("w-full"):
-    cursor = conn.cursor()
+cursor = conn.cursor()
 
-    with ui.list().props("bordered separator"):
-        ui.item_label("Tables").props("header").classes("text-bold")
-        ui.separator()
+cols = [
+    {"name": "id", "label": "ID", "field": "id"},
+    {"name": "name", "label": "Name", "field": "name"},
+    {"name": "url", "label": "BaseUrl", "field": "url"},
+    {"name": "key", "label": "ApiKey", "field": "key"},
+    {"name": "used", "label": "Used", "field": "used"},
+    {"name": "rest", "label": "Rest", "field": "rest"},
+    {"name": "speed", "label": "Speed", "field": "speed"},
+]
 
-        cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
+params = {
+    "align": "left",
+    "sortable": True,
+    "headerClasses": "text-primary",
+}
 
-        for row in cursor.fetchall():
-            with ui.item().classes("items-center"):
-                ui.item_label(row[0])
+with ui.table(rows=[], columns=cols, column_defaults=params).classes("w-full") as table:
+    cursor.execute(
+        "SELECT id, name, base_url, key, used_quota, balance, response_time FROM channels;"
+    )
 
-    cols = [
-        {"name": "id", "label": "ID", "field": "id"},
-        {"name": "name", "label": "Name", "field": "name"},
-        {"name": "url", "label": "BaseUrl", "field": "url"},
-        {"name": "key", "label": "ApiKey", "field": "key"},
-        {"name": "speed", "label": "Speed", "field": "speed"},
-    ]
-    params = {
-        "align": "left",
-        "sortable": True,
-        "headerClasses": "text-primary",
-    }
-    with ui.table(rows=[], columns=cols, column_defaults=params).classes(
-        "flex-1"
-    ) as table:
-        cursor.execute("SELECT id, name, base_url, key, response_time FROM channels;")
-
-        for row in cursor.fetchall():
-            speed = f"{row[4] / 1000:.2f}s"
-            table.add_row(
-                {
-                    "id": row[0],
-                    "name": row[1],
-                    "url": row[2],
-                    "key": row[3],
-                    "speed": speed,
-                }
-            )
-
-    cursor.close()
+    for row in cursor.fetchall():
+        used = row[4] / 500000
+        speed = f"{row[6] / 1000:.2f}s"
+        rest = row[5] - used if row[5] else 0
+        table.add_row(
+            {
+                "id": row[0],
+                "name": row[1],
+                "url": row[2],
+                "key": row[3],
+                "used": used,
+                "rest": rest,
+                "speed": speed,
+            }
+        )
 
 ui.run(port=8888)
