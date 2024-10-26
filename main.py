fab7451d9074a6e272750e9e2aacc6d3daeed97d
Arthur63 <arthur63@163.com>
2024-10-26 17:48:02 +0800

    refactor ui

--- main.py
+++ main.py
@@ -4,17 +4,32 @@ from nicegui import ui
 conn = sqlite3.connect("one-api.db")
 conn.row_factory = sqlite3.Row
 
-with ui.header().classes("justify-center"):
+with ui.header().classes("items-center"):
+    ui.button(on_click=lambda: drawer.toggle(), icon="menu").props("flat color=white")
     ui.label("OneAPI Tool").classes("text-xl text-bold")
 
+with ui.left_drawer(value=False).props("bordered") as drawer:
+    with ui.list().props("bordered seprator").classes("w-full"):
+        with ui.item(on_click=lambda: page_switch(table)):
+            with ui.item_section().props("avatar"):
+                ui.icon("table_chart", color="primary")
+            with ui.item_section():
+                ui.label("Channels")
+        with ui.item(on_click=lambda: page_switch(info)):
+            with ui.item_section().props("avatar"):
+                ui.icon("analytics", color="primary")
+            with ui.item_section():
+                ui.label("Information")
+
+
 cursor = conn.cursor()
-rows = {}
+rows = cursor.execute("SELECT * FROM channels;").fetchall()
 
 cols = [
     {"name": "id", "label": "ID", "field": "id"},
     {"name": "name", "label": "Name", "field": "name"},
-    {"name": "url", "label": "BaseUrl", "field": "url"},
-    {"name": "key", "label": "ApiKey", "field": "key"},
+    {"name": "url", "label": "BaseURL", "field": "url"},
+    {"name": "key", "label": "APIKey", "field": "key"},
     {"name": "used", "label": "Used", "field": "used"},
     {"name": "rest", "label": "Rest", "field": "rest"},
     {"name": "speed", "label": "Speed", "field": "speed"},
@@ -23,7 +38,6 @@ cols = [
 params = {
     "rows": [],
     "columns": cols,
-    "selection": "single",
     "column_defaults": {
         "align": "left",
         "sortable": True,
@@ -32,10 +46,7 @@ params = {
 }
 
 with ui.table(**params).props("flat bordered").classes("w-full") as table:
-    cursor.execute("SELECT * FROM channels;")
-
-    for row in cursor.fetchall():
-        rows[row["id"]] = row
+    for row in rows:
         used = row["used_quota"] / 500000
         speed = f"{row['response_time'] / 1000:.2f}s"
         rest = row["balance"] - used if row["balance"] else 0
@@ -52,11 +63,23 @@ with ui.table(**params).props("flat bordered").classes("w-full") as table:
             }
         )
 
-
-with ui.card().props("flat bordered").classes("w-full"):
+with ui.card().props("flat bordered").classes("w-full") as info:
     ui.label("Channel Information").classes("text-base text-bold")
 
+    def on_change(e):
+        row = next((r for r in rows if r["name"] == e.value))
+        edit_mapping.set_value(row["model_mapping"])
+        edit_models.set_value(row["models"])
+        edit_url.set_value(row["base_url"])
+        edit_name.set_value(row["name"])
+        edit_key.set_value(row["key"])
+
     with ui.row().classes("w-full justify-center"):
+        with ui.column():
+            ui.label("Channel")
+            ui.select(
+                options=[r["name"] for r in rows], on_change=on_change, with_input=True
+            ).props("outlined dense")
         with ui.column():
             ui.label("Channel Nmae")
             edit_name = ui.input().props("outlined dense")
@@ -76,25 +99,12 @@ with ui.card().props("flat bordered").classes("w-full"):
             edit_mapping = ui.textarea().props("outlined dense").classes("w-full")
 
 
-def on_select(ev):
-    if len(selection := ev.selection) <= 0:
-        edit_mapping.set_value(None)
-        edit_models.set_value(None)
-        edit_name.set_value(None)
-        edit_url.set_value(None)
-        edit_key.set_value(None)
-        return
-
-    row = rows[selection[0]["id"]]
-
-    edit_key.set_value(row["key"])
-    edit_name.set_value(row["name"])
-    edit_url.set_value(row["base_url"])
-
-    edit_models.set_value(row["models"])
-    edit_mapping.set_value(row["model_mapping"])
+def page_switch(page):
+    [p.set_visibility(False) for p in pages]
+    page.set_visibility(True)
 
 
-table.on_select(on_select)
+pages = [table, info]
+page_switch(table)
 
 ui.run(port=8888)
